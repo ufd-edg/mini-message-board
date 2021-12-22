ce9ba1be774ff35409fba12ce69d20df90291eb3
UglyFatDead <julianviceroy124@gmail.com>
2021-12-22 20:10:18 -0300

    commit

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: "Mini Messageboard", messages: messages })
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('form', { title: 'New Message' })
+});
+
+router.post('/new', (req, res, next) => {
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.authorName,
+    added: new Date()
+  });
+  res.redirect('/')
+});
+
+module.exports = router;
